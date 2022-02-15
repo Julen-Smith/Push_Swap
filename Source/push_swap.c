2fc18ad7512d114f521eba90142b920a69c4e6fa
Julen Smith <jsmith@c3r6s2.42urduliz.com>
2022-02-15 12:13:00 +0100

    ls

--- Source/push_swap.c
+++ Source/push_swap.c
@@ -6,74 +6,112 @@
 /*   By: jsmith <jsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 07:57:04 by jsmith            #+#    #+#             */
-/*   Updated: 2022/02/13 03:16:51 by jsmith           ###   ########.fr       */
+/*   Updated: 2022/02/15 12:12:33 by jsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Include/push_swap.h"
 
-
-int	ft_find_lowerest_node(t_nodelst *lst)
+t_node *ft_return_lowst_pointer(t_nodelst *lst)
 {
-	t_node *point;
-	int lowst;
-	t_node *actualize_pointer;
-	
+	t_node *pnt;
+	t_node *dummy;
 
-	point = lst->a_head;
-	lowst = point->nbr;
-	while(point->next)
+	pnt = lst->a_head;
+	dummy = lst->a_head;
+	while (pnt->next)
 	{
-		if (point->nbr < lowst)
-		{
-			lowst = point->nbr;
-			actualize_pointer = point;
-		}
-			
-		point = point->next;
+		if (pnt->nbr < dummy->nbr)
+			dummy = pnt;
+		pnt = pnt->next;
 	}
-	actualize_pointer->position = 1;
-	return (lowst);
+	if (pnt->nbr < dummy->nbr)
+		dummy = pnt;
+	dummy->position = 1;
+	return (dummy);
+}
+
+t_node *ft_return_biggst_pointer(t_nodelst *lst)
+{
+	t_node *pnt;
+	t_node *dummy;
+
+	pnt = lst->a_head;
+	dummy = lst->a_head;
+	while (pnt->next)
+	{
+		if (pnt->nbr > dummy->nbr)
+			dummy = pnt;
+		pnt = pnt->next;
+	}
+	if (pnt->nbr > dummy->nbr)
+		dummy = pnt;
+	dummy->position = ft_iterate_stack(lst,'a') +1;
+	return (dummy);
+}
+
+t_node	*ft_order(t_nodelst *lst, t_node *actual_node, int i)
+{
+	t_node *hp;
+	t_node *target;
+
+	hp = lst->a_head;
+	while(hp->next)
+	{	
+		while(hp->nbr < actual_node->nbr && hp->next)
+		{
+				printf("Acelero posiciones por ser menores a la buscada\n");
+				printf("%d\n",hp->nbr);
+				hp = hp->next;
+			
+		}
+			if (!hp->next)
+			{
+				printf("f\n");
+				return (0);
+			}
+				
+			printf("Acelero posiciones por ser menores a la buscada\n");
+			//hp = hp->next;
+		if (hp->nbr > actual_node->nbr)
+			printf("Se descubre nuevo numero superior\n");
+			//target = hp;
+		if(hp->nbr < target->nbr)
+			printf("Si se descubre un numero numero entre los dos se asigna\n");
+			//target = hp;
+		hp = hp->next;
+	}	
+	return (hp);
 }
 
 void	ft_generate_positions(t_nodelst *lst)
 {
+	t_node *low;
+	t_node *hp;
+	t_node *dummy;
 	int i;
-	int quant;
-	t_node *point;
-	int lowst;
-	int cache;
-	t_node *actualize_pointer;
-
-	i = 2;
-	cache = 2147483647;
-	quant = ft_iterate_stack(lst,'a') + 1;
-	lowst = ft_find_lowerest_node(lst);
-	actualize_pointer = lst->a_head;
-	while(i != quant)
+	
+	i = 2;	
+	low	= ft_return_lowst_pointer(lst);
+	hp = lst->a_head;
+	while(hp->next)
 	{
-		point = lst->a_head;
-		while(point->next)
-		{
-			if (point->nbr < cache  && point->nbr > actualize_pointer->nbr)
-			{
-				cache = point->nbr;
-				actualize_pointer = point;	
-			}	
-			point = point->next;
-		}
-		actualize_pointer->position = i;
+		printf("Buscando el numero = %d\n", i);
+		dummy = ft_order(lst, hp, i);
+		dummy->position = i;
+		hp = hp->next;
 		i++;
-	}	
+	}
 }
+
 
 int main(int argc, char *argv[])
 {
 	t_nodelst *nodelst;
 	
 	nodelst = ft_manage_entry(argc, argv, nodelst);
+	// ft_find_lowerest_node(nodelst);
 	ft_generate_positions(nodelst);
-	
 	ft_print_stack_a(nodelst);
 	ft_print_stack_b(nodelst);
     return (0);
@@ -82,7 +120,7 @@ int main(int argc, char *argv[])
 	
 
 /*
-	printf("\n------------------------------\n\n");
+	printf("\n-----------------------------\n\n");
 	ft_print_stack_a(nodelst);
 	ft_print_stack_b(nodelst);
 	ft_push_b(nodelst);
